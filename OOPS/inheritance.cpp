ad570f9cd3c96b64cfcac2f1389a38ca537669cc
Suhani-024 <suhanikapoor024@gmail.com>
2026-07-21 22:58:07 +0530

    Inheritance

--- OOPS/inheritance.cpp
+++ OOPS/inheritance.cpp
@@ -2,8 +2,57 @@
 using namespace std;
 
 class Teacher{
-    public:
+public:
     string name;
+    int age;
+//parameterized
+Teacher(string name,int age){
+        this->name=name;
+        this->age=age;
+    }
+
+   //non parameterized
+    Teacher(){
+       cout<<"parent constructor"<<endl;
+    }
+
     
 
+
 };
+
+class Student : public Teacher{
+    //name,age,roll no.
+
+    public:
+    int rollno;
+//parameterized
+    Student(string name,int age,int rollno) : Teacher(name, age){
+        this->rollno=rollno;
+    }
+
+
+//non parameterized
+    Student(){
+        cout<<"child constructor"<<endl;
+
+    }
+
+    void getInfo(){
+        cout <<"name:" <<name<<endl;
+        cout<<"age:" <<age<<endl;
+        cout<<"rollno"<<rollno<<endl;
+    }
+
+
+};
+int main(){
+
+    Student s1;
+    s1.name="Suhani";
+    s1.age=21;
+    s1.rollno=20;
+    s1.getInfo();
+
+    return 0;
+}
